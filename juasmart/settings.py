fc26bbe53c40e38749a3aa5c8b6e485a8fc9000b
Charles Muthui <126247035+charlesricha@users.noreply.github.com>
2025-07-04 21:36:26 +0300

    allowed render as a host

--- juasmart/settings.py
+++ juasmart/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+# ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['jua-smart.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
